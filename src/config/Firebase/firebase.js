163b3fea65c5b88c4863b6dd0081cc8082df5d0d
Lahiru Epa <lahiruepa@gmail.com>
2020-06-24 21:31:17 +0530

    removed firebase credentials

--- src/config/Firebase/firebase.js
+++ src/config/Firebase/firebase.js
@@ -2,14 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBMk_gT4FDWVwvLQOkwsIfAdYZxM5hr8Qw",
-    authDomain: "project-whm-1dd03.firebaseapp.com",
-    databaseURL: "https://project-whm-1dd03.firebaseio.com",
-    projectId: "project-whm-1dd03",
-    storageBucket: "project-whm-1dd03.appspot.com",
-    messagingSenderId: "444783523754",
-    appId: "1:444783523754:web:db044e24c7d83095b85dd3",
-    measurementId: "G-17SKX1S6J4"
+    //add config details
 };
 
 class Firebase {
